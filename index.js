52a2feba29d2c9eab127e89576a163504bbedeb1
Ian <lolsugotpwnd@yahoo.com>
2020-01-16 17:26:46 -0600

    adjusted port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server.js');
 
-// const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(`\n*** Server Running on https://guarded-lake-21552.herokuapp.com/ ***\n`);
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
